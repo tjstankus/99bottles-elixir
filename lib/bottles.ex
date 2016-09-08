135d7485c370b8b2f3f2d0ed91184c132d80f323
TJ Stankus <tjstankus@gmail.com>
2016-09-07 21:55:46 -0400

    Implement first verse

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -1,2 +1,8 @@
 defmodule Bottles do
+  def verse(_) do
+    "99 bottles of beer on the wall, " <>
+      "99 bottles of beer.\n" <>
+      "Take one down and pass it around, " <>
+      "98 bottles of beer on the wall.\n"
+  end
 end
--- test/bottles_test.exs
+++ test/bottles_test.exs
@@ -2,7 +2,11 @@ defmodule BottlesTest do
   use ExUnit.Case
   doctest Bottles
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "first verse" do
+    expected = "99 bottles of beer on the wall, " <>
+      "99 bottles of beer.\n" <>
+      "Take one down and pass it around, " <>
+      "98 bottles of beer on the wall.\n"
+    assert expected == Bottles.verse(99)
   end
 end
