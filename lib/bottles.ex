af2fb64616b05e768439b0bc2fd1b088733aa0c8
TJ Stankus <tjstankus@gmail.com>
2016-09-07 22:17:22 -0400

    Implement another verse

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -1,8 +1,16 @@
 defmodule Bottles do
-  def verse(_) do
-    "99 bottles of beer on the wall, " <>
-      "99 bottles of beer.\n" <>
-      "Take one down and pass it around, " <>
-      "98 bottles of beer on the wall.\n"
+  def verse(number) do
+    case number do
+      99 ->
+        "99 bottles of beer on the wall, " <>
+          "99 bottles of beer.\n" <>
+          "Take one down and pass it around, " <>
+          "98 bottles of beer on the wall.\n"
+      3 ->
+        "3 bottles of beer on the wall, " <>
+          "3 bottles of beer.\n" <>
+          "Take one down and pass it around, " <>
+          "2 bottles of beer on the wall.\n"
+    end
   end
 end
--- test/bottles_test.exs
+++ test/bottles_test.exs
@@ -9,4 +9,12 @@ defmodule BottlesTest do
       "98 bottles of beer on the wall.\n"
     assert expected == Bottles.verse(99)
   end
+
+  test "another verse" do
+    expected = "3 bottles of beer on the wall, " <>
+      "3 bottles of beer.\n" <>
+      "Take one down and pass it around, " <>
+      "2 bottles of beer on the wall.\n"
+    assert expected == Bottles.verse(3)
+  end
 end
