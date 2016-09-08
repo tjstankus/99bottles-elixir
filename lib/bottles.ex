a79489fa8e9c4901f5180c820bf0fe635bbf66d3
TJ Stankus <tjstankus@gmail.com>
2016-09-08 17:54:13 -0400

    Reorder, align for refactoring duplication

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -9,26 +9,14 @@ defmodule Bottles do
 
   def verse(number) do
     case number do
-      2 ->
-        "2 bottles of beer on the wall, " <>
-          "2 bottles of beer.\n" <>
-          "Take one down and pass it around, " <>
-          "1 bottle of beer on the wall.\n"
-      1 ->
-        "1 bottle of beer on the wall, " <>
-          "1 bottle of beer.\n" <>
-          "Take it down and pass it around, " <>
-          "no more bottles of beer on the wall.\n"
       0 ->
-        "No more bottles of beer on the wall, " <>
-          "no more bottles of beer.\n" <>
-          "Go to the store and buy some more, " <>
-          "99 bottles of beer on the wall.\n"
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      1 ->
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      2 ->
+        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
       _ ->
-        "#{number} bottles of beer on the wall, " <>
-          "#{number} bottles of beer.\n" <>
-          "Take one down and pass it around, " <>
-          "#{number - 1} bottles of beer on the wall.\n"
+        "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     end
   end
 end
